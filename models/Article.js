5a8381c79432fd54878ef699bd360392554f1d4b
Raywire <ryanwire@outlook.com>
2019-09-05 20:05:15 +0300

    Adds favoriting

--- models/Article.js
+++ models/Article.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 const slug = require('slug');
+const User = mongoose.model('User');
 
 const ArticleSchema = new mongoose.Schema({
   slug: {type: String, lowercase: true, unique: true},
@@ -24,6 +25,16 @@ ArticleSchema.methods.slugify = function() {
   this.slug = slug(this.title);
 };
 
+ArticleSchema.methods.updateFavoriteCount = function() {
+  var article = this;
+
+  return User.count({favorites: {$in: [article._id]}}).then(function(count){
+    article.favoritesCount = count;
+
+    return article.save();
+  });
+};
+
 ArticleSchema.methods.toJSONFor = function(user){
   return {
     slug: this.slug,
@@ -33,6 +44,7 @@ ArticleSchema.methods.toJSONFor = function(user){
     createdAt: this.createdAt,
     updatedAt: this.updatedAt,
     tagList: this.tagList,
+    favorited: user ? user.isFavorite(this._id) : false,
     favoritesCount: this.favoritesCount,
     author: this.author.toProfileJSONFor(user)
   };
