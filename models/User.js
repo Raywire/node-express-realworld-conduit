dcca6085a78d57557d60ecd557e92f9ec3bad8de
Raywire <ryanwire@outlook.com>
2019-09-07 17:28:33 +0300

    Replaces deprecated crypto with bcrypt

--- models/User.js
+++ models/User.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
-const crypto = require('crypto')
+const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
-const secret = require('../config').secret
+const { secret } = require('../config')
 
 const UserSchema = new mongoose.Schema({
   username: { type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true },
@@ -12,20 +12,18 @@ const UserSchema = new mongoose.Schema({
   favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Article' }],
   following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
   admin: { type: Boolean, default: false },
-  hash: String,
-  salt: String
+  hash: String
 }, { timestamps: true })
 
 UserSchema.plugin(uniqueValidator, { message: 'is already taken.' })
 
-UserSchema.methods.validPassword = function (password) {
-  const hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex')
-  return this.hash === hash
+UserSchema.methods.validPassword = async function (password) {
+  const match = await bcrypt.compare(password, this.hash)
+  return match
 }
 
-UserSchema.methods.setPassword = function (password) {
-  this.salt = crypto.randomBytes(16).toString('hex')
-  this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex')
+UserSchema.methods.setPassword = async function (password) {
+  this.hash = await bcrypt.hash(password, 10)
 }
 
 UserSchema.methods.generateJWT = function () {
